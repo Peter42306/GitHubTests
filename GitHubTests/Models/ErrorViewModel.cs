2a69d53c7759b79a6069b01f8d343fd6d2e4cf4d
Petr Zalizko <pzalizko@gmail.com>
2024-07-07 12:12:49 +0300

    Add project files.

---
+++ GitHubTests/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHubTests.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
